8edb51a52c5947998aa4a354b5c0fb0fe72e7781
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-06-06 16:28:35 +0200

    first commit

---
+++ myLibrary-c#/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Coding.Exercise
+{
+    public static class MathConstants
+    {
+        public const double PI = 3.141592653589793;
+        public static double GoldenRatio { get; } = 1.61803398875;
+    }
+
+    public class Exercise
+    {
+       
+        public void PrintConstants()
+        {
+            Console.WriteLine($"PI: {MathConstants.PI}");
+            Console.WriteLine($"Golden Ratio: {MathConstants.GoldenRatio}");
+        }
+    }
+}
